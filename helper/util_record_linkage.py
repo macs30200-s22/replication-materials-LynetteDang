ea863b536cda33dd8b4b1a3e7107aa0ea381ee57
Lynette Dang <lidang@uchicago.edu>
2022-05-01 22:23:48 -0500

    final network/regression analysis added

--- helper/util_record_linkage.py
+++ helper/util_record_linkage.py
@@ -2,8 +2,8 @@
 Name: util_record_linkage.py
 Author: Lynette Dang
 
-Utility function to help link the LES dataset and the connection dictionary
-using
+Utility function to help merge the LES dataset and the connection dictionaries
+using record linkage
 """
 import pandas as pd
 import fuzzy_pandas as fpd
@@ -11,20 +11,36 @@ import json
 
 house = pd.read_excel("House116.xlsx")
 senate = pd.read_excel("Senate116.xlsx")
-with open('ind_network.json') as json_file:
-    ind_network = json.load(json_file)
 house = house[['Legislator name', 'Legislative Effectiveness Score']]
 senate = senate[['Legislator name', 'Legislative Effectiveness Score']]
 df_les = pd.concat([house, senate], axis=0)
-df_conn = pd.DataFrame(ind_network.items(), columns=[
-                       "Legislator name", "Number of Connections"])
-# df_analysis contain information of 480 legislators,
-# I have manually checked for these 480 match,
-# but will have to go back and check for the 535-480 = 55 missing legislators
-# in the future
-df_analysis = fpd.fuzzy_merge(df_conn, df_les,
-                              left_on='Legislator name',
-                              right_on='Legislator name',
-                              method='levenshtein',
-                              threshold=0.80)
-df_analysis.to_csv("analysis.csv")
+with open('conn_network.json') as json_file:
+    conn_network = json.load(json_file)
+with open('ind_network.json') as json_file_2:
+    ind_network = json.load(json_file_2)
+
+
+def record_link(dic, df2, threshold, name):
+    """
+    Merge a dictionary with the LES dataset using record linkage
+
+    Inputs:
+        dic: dictionary object
+        df2: dataframe containing the LES dataset
+        threshold: threshold for record linkage
+        name: name of the output csv file
+    Outputs:
+        /
+    """
+    df1 = pd.DataFrame(dic.items(), columns=[
+        "Legislator name", "Connections"])
+    df_analysis = fpd.fuzzy_merge(df1, df2,
+                                  left_on='Legislator name',
+                                  right_on='Legislator name',
+                                  method='levenshtein',
+                                  threshold=threshold)
+    df_analysis.to_csv(name+".csv")
+
+
+record_link(conn_network, df_les, 0.80, 'analysis_conn')
+record_link(ind_network, df_les, 0.80, 'analysis_ind')
